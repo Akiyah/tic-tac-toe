3dc16be2a85977c4de952b25fe86c07a1e425a9b
akiya.mizukoshi <akiya.mizukoshi@naviplus.co.jp>
2022-03-13 17:07:32 +0900

    add #key test

--- test/js/modules/board.test.js
+++ test/js/modules/board.test.js
@@ -11,3 +11,26 @@ test('constructor', () => {
   ]);
 });
 
+test('#step', () => {
+  let board = new Board();
+  board = board.step(1, 2, 'o');
+  expect(board.tiles).toEqual([
+    [new Tile(0, 0), new Tile(1, 0), new Tile(2, 0)],
+    [new Tile(0, 1), new Tile(1, 1), new Tile(2, 1)],
+    [new Tile(0, 2), new Tile(1, 2, 'o'), new Tile(2, 2)],
+  ]);
+
+  board = board.step(0, 1, 'x');
+  expect(board.tiles).toEqual([
+    [new Tile(0, 0), new Tile(1, 0), new Tile(2, 0)],
+    [new Tile(0, 1, 'x'), new Tile(1, 1), new Tile(2, 1)],
+    [new Tile(0, 2), new Tile(1, 2, 'o'), new Tile(2, 2)],
+  ]);
+});
+
+test('#key', () => {
+  let board = new Board();
+  board = board.step(1, 2, 'o');
+  board = board.step(0, 1, 'x');
+  expect(board.key()).toEqual("   \nx  \n o ");
+});
