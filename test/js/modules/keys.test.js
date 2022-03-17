342e22fcc365e4160308c579c366cf2006ec27d9
akiya.mizukoshi <akiya.mizukoshi@naviplus.co.jp>
2022-03-18 02:37:51 +0900

    add keys

---
+++ test/js/modules/keys.test.js
@@ -0,0 +1,15 @@
+const KEYS = require('../../../src/js/modules/keys');
+
+test('length', () => {
+  expect(KEYS[0].length).toBe(1);
+  expect(KEYS[1].length).toBe(3);
+  expect(KEYS[2].length).toBe(12);
+  expect(KEYS[3].length).toBe(38);
+  expect(KEYS[4].length).toBe(108);
+  expect(KEYS[5].length).toBe(174);
+  expect(KEYS[6].length).toBe(204);
+  expect(KEYS[7].length).toBe(153);
+  expect(KEYS[8].length).toBe(57);
+  expect(KEYS[9].length).toBe(15);
+  expect(KEYS.length).toBe(10);
+});
